d0b4b4c9ae8930037e1d0a4af9ca2429e48773ac
Maseongbin <125714011+Maseongbin@users.noreply.github.com>
2023-09-04 16:47:37 +0900

    Add files via upload

---
+++ 20230904_7/20230904_7/main.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable:4996)
+
+struct Node 
+{
+	int a;
+	double b;
+};
+
+int main()
+{
+	struct Node* print;
+	FILE* fp;
+	fp = fopen("data.txt", "wb");
+
+	print = (struct Node*)malloc(sizeof(struct Node));
+
+	print->a = 1;
+
+	print->b = 3.14;
+
+	fwrite((void*)print, sizeof(struct Node), 1, fp);
+
+	fclose(fp); 
+
+	free(print); 
+}
